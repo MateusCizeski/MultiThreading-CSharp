c5b6bc80a1a368e989bb4993b9b838551b7448c3
mateu <mateuscizeski@gmail.com>
2024-12-25 19:51:17 -0300

    Exemplo de thread.

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var thread1 = new Thread(ProcessoA);
+        var thread2 = new Thread(ProcessoB);
+
+        thread1.Start();
+        thread2.Start();
+
+        Console.ReadLine();
+    }
+
+    public static void ProcessoA()
+    {
+        var items = new String[10] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+        for(int i = 0; i < items.Length; i++)
+        {
+            Console.WriteLine("Processo A => " + items[i]);
+            if(i == 5)
+            {
+                Thread.Sleep(2000);
+            }
+        }
+    }
+    
+    public static void ProcessoB()
+    {
+        var items = new String[10] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            Console.WriteLine("Processo B => " + items[i]);
+        }
+    }
+}
